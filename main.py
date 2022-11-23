40443c5e496b23d939e85fdac73361bb58998639
18 MM <114921866+Meleko2@users.noreply.github.com>
2022-11-23 06:39:43 +0000

    Dodanie algorytmow

--- main.py
+++ main.py
@@ -1,33 +1,68 @@
-#zad 1
-a=int(input())
-if a % 3 == 0:
-  print("tak")
+#algorytm sprawdzajacy czy liczba jest pierwsza
+# wersja 1
+n = int(input())
+for i in range(2,n):
+  if n%i==0:
+    print("Liczba nie jest pierwsza")
+    exit(0)
+print("Liczba jest pierwsza")
+
+#wersja 2
+flaga = True
+for i in range(2,n):
+  if i%n==0:
+    flaga=False
+if flaga == True:
+  print("Licbza jest pierwsza")
 else:
-  print('nie')
-#zad 2
-a = int(input())
-if a%17 == 0 and a>=100 and a<1000:
-  print('tak')
-else:
-  print('nie')
-#zad 3
-wiek=int(input("Ile masz lat"))
-if wiek>=18:
-  print('tak')
-else:
-  print('nie')
-#zad 4
-limit=20
-waga = int(input())
-if waga>limit:
-    print('nie')
-else:
-    print('tak')
-#zad 5
-a = int(input())
-b = int(input())
-c = int(input())
-if a < c < b or a > c > b :
-    print("TAK")
-else :
-    print("NIE")
+  print("Liczba nie jest pierwsza")
+
+#Wersja 3-the best
+from math import sqrt
+n = int(input())
+for i in range(2, int(sqrt(n))+1):
+  if n%i==0:
+    print("Liczba nie jest pierwsza")
+  else:
+    print("Liczba jest pierwsza")
+  
+#2. generator liczb pierwszych- liczby pierwsze w przewdziale[p, q]
+p = int(input())
+q = int(input())
+for i in range(p, q+1):
+  flaga = True
+  for j in range(2,int(i**0,5)+1):
+    if i%j == 0:
+      flaga = False
+    if flaga:
+      print(i, end=" ")
+
+#3. generator liczb pierwszych - poczatkowe k liczb pierwszych
+k = int(input())
+while True:
+  k-=1
+  if k == 0:
+    break
+
+
+#3-2 sposob
+n = int(input())
+x=2
+while 1:
+  flaga = True
+  for i in range(2, int(x**0.5)+1):
+    if x%i == 0:
+      flaga=False
+    if flaga:
+      print(x, end=" ")
+      
+
+
+    
+  
+    
+
+
+
+
+
