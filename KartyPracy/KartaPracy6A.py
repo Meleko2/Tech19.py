b31b3e3c5e0b35fafd250b4fc07aabe79d5d864e
17 MM <114921866+Meleko2@users.noreply.github.com>
2022-12-17 21:51:42 +0000

    Dodano cześciowe rozwiązanie KartyPracy6A

--- KartyPracy/KartaPracy6A.py
+++ KartyPracy/KartaPracy6A.py
@@ -1,5 +1,12 @@
+def NWD(a,b):
+  while a!=b:
+    if a>b:
+      a-=b
+    else:
+      b-=a
+
 #Zadania 1 - do zrozumienia
-x = int(input())
+x = int(input("Podaj liczbe: "))
 suma = 0
 while x > 0:
   suma += x % 10
@@ -8,7 +15,7 @@ while x > 0:
 print(suma)
 
 #Zadania 2
-x = int(input())
+x = int(input("Podaj liczbe: "))
 temp = 0
 for i in range(1,x):
   if x%i == 0:
@@ -19,7 +26,7 @@ else:
   print("nie")
     
 #Zadania 3 - doskonale- dzielniki dodane do siebie daja ta liczbe nie liczac jej samej
-n = int(input())
+n = int(input("Podaj liczbe: "))
 suma = 0
 for i in range(1,n):
   if i%n == 0:
@@ -30,37 +37,40 @@ else:
   print("Nie jest doskonala")
 
 #Zadania 4 - wzglednie pierwsze- ich nwd = 1
-x = int(input())
-y = int(input())
-while x!=y:
-  if x>y:
-    x-=y
-  if y>x:
-    y-=x
+x = int(input("Podaj liczbe1: "))
+y = int(input("Podaj liczbe2: "))
+NWD(x, y)
 if x==1:
   print("Liczby sa wzglednie pierwsze")
 else:
   print("Liczby nie sa wzglednie pierwsze")
 
 #Zadania 5
-m = int(input())
+m = int(input("Podaj liczbe: "))
 for i in range(10,20):
   x = m
   y = i
-  while y>0:
-    x,y=y, x%y
+  NWD(x, y)
   if x==1:
-    print(f"Pasuje dla: {m}, {i}")
+    print(f"Wzglednie pierwsze: {m}, {i}")
     
 #Zadania 6
-a=int(input())
-b=int(input())
+a=int(input("Licznik: "))
+b=int(input("Mianownik: "))
+c=a
+d=b
 a,b=x,y
-while x!=y:
-  if x>y:
-    x-=y
-  else:
-    y-=x
+NWD(x, y)
 a//=x
 b//=y
-print(f"Skrocony ulamek {a}/{b}")
+print(f"Skrocony ulamek {c}/{d} == {a}/{b}")
+
+#Zadania 7
+a = int(input("Licznik: "))
+b = int(input("Mianownik: "))
+calosci=a//b
+reszta=a%b
+if reszta==0:
+  print(f"Ulamek niewlasciwy to: {calosci}")
+else:
+  print(f"Ulamek niewlasciwy to: {calosci} i {reszta}/{b}")
